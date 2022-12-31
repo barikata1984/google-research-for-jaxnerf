427e81681bd688c811c8d208fb5c9357df70aaf4
Barikata1984 <franche1984@gmail.com>
2022-12-31 17:41:46 -0500

    initial commit

--- jaxnerf/train.sh
+++ jaxnerf/train.sh
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# comment added to confirm the initial commit
+
 #!/bin/bash
 CONFIG=$1
 DATA_ROOT=$2
